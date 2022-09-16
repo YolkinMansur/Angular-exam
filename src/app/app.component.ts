c5a165c2a299230803ded4d15ac41ff16aa5d5fe
YolkinMansur <yolkinmansur@gmail.com>
2022-09-16 22:57:08 +0600

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularToDo';
+  public tasks: Task[] = [];
+  public text: string = '';
+  addTasks(text: any) {
+    if (text !== '') {
+      this.tasks.push(text);
+      this.text = '';
+      console.log(this.tasks);
+    } else {
+      return;
+    }
+  }
+  deleteTasks(task: any) {
+    this.tasks = this.tasks.filter((item) => item !== task);
+  }
 }
